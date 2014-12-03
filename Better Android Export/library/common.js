25d3bba1967108fe592d105ae0457108ccc05385
Nick Stamas <nicholas.stamas@gmail.com>
2014-12-03 15:18:51 -0500

    added support for exporting nested layers

---
+++ Better Android Export/library/common.js
@@ -0,0 +1,83 @@
+#import 'library/sandbox.js'
+
+var com = {};
+com.animal = {
+  exportWithFactors: function(factors) {
+    if ([selection count] == 0) {
+      [doc showMessage:"No layer is selected."];
+    } else {
+      var openDlg = [NSOpenPanel openPanel];
+
+      // Enable the selection of files in the dialog.
+      [openDlg setCanChooseFiles:false];
+      [openDlg setCanChooseDirectories:true];
+      [openDlg setAllowsMultipleSelection:false];
+      [openDlg setPrompt:"Select"];
+
+      if ( [openDlg runModalForDirectory:nil file:nil] == NSOKButton ) {
+
+        for (var j=0; j < [selection count]; j++) {
+          
+          var layer = [selection objectAtIndex:j];
+          var artboard = [layer parentArtboard];
+          var layerVisibility = [];
+
+          [artboard deselectAllLayers];
+          log(selection);
+          var layerArray = [layer];
+          [artboard selectLayers:layerArray];
+
+          var root = artboard;
+
+          var hideLayers = function(root, target) {
+            // Hide all layers except for selected and store visibility
+            for (var k=0; k < [[root layers] count]; k++) {
+              var currentLayer = [[root layers] objectAtIndex:k];
+              if ([currentLayer containsSelectedItem]) {
+                hideLayers(currentLayer, target);
+              } else if (!(currentLayer == target)) {
+                var dict = [[NSMutableDictionary alloc] init];
+                [dict addObject:currentLayer forKey:"layer"];
+                [dict addObject:[currentLayer isVisible] forKey:"visible"];
+                  
+                layerVisibility.push(dict);
+                [currentLayer setIsVisible: false];
+              }
+            }
+          }
+
+          var rect = [[layer absoluteRect] rect];
+          var path = [[[openDlg URLs] objectAtIndex:0] fileSystemRepresentation];
+
+          new AppSandbox().authorize(path, function() {
+            for (var f=0; f < factors.length; f++) {
+              var factor = factors[f];
+              slice = [MSExportRequest requestWithRect:rect scale:factor["scale"]];
+              [doc saveArtboardOrSlice:slice toFile:path + "/" + factor["name"] + "/" + [layer name] + ".png"]; 
+            }
+          });
+
+          // Restore layers visibility
+          for (var m=0; m < layerVisibility.length; m++) {
+            var dict = layerVisibility[m];
+            var layer = [dict objectForKey:"layer"];
+            var visibility = [dict objectForKey:"visible"];
+            
+            if (visibility == 0) {
+              [layer setIsVisible:false];
+            } else {
+              log("visible is true");
+              [layer setIsVisible:true];
+            }
+          }
+
+          // Restore selection
+          [artboard selectLayers:selection];
+
+        }
+        [doc showMessage: [selection count] + " layers exported to " + path]; 
+      }
+    }
+  },
+
+}
